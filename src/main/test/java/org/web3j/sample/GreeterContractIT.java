71ea8b529668bf4ae5656e6077d6510f12dcf889
rene-huber <13349644+axa-huembi@users.noreply.github.com>
2019-11-14 17:06:31 +0100

    add a invalid GitHub token

--- src/main/test/java/org/web3j/sample/GreeterContractIT.java
+++ src/main/test/java/org/web3j/sample/GreeterContractIT.java
@@ -6,7 +6,7 @@ import org.junit.Test;
  * Integration test to run our main application.
  */
 public class GreeterContractIT {
-
+    public static final String token = "60511bc11111f111c11f11d11111ae1111d1e11b";
     @Test
     public void testGreeterContract() throws Exception {
         Application.main(new String[]{ });
