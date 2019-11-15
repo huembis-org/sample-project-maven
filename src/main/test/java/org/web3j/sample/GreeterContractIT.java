f0be4ae0b3f6ff848b806e701a05a2c8815f8c77
rene-huber <13349644+axa-huembi@users.noreply.github.com>
2019-11-15 08:13:59 +0100

    test with real preprod token

--- src/main/test/java/org/web3j/sample/GreeterContractIT.java
+++ src/main/test/java/org/web3j/sample/GreeterContractIT.java
@@ -7,6 +7,8 @@ import org.junit.Test;
  */
 public class GreeterContractIT {
     public static final String token = "60511bc11111f111c11f11d11111ae1111d1e11b";
+    public static final String preprodToken ="2770a8bc696fc8408e7d7c331f318b916b330654";
+    
     @Test
     public void testGreeterContract() throws Exception {
         Application.main(new String[]{ });
